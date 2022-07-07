ca7cf51e693f2bb5c1e555cbcc58cf7bc6dcc09f
Artem Eroshenko <eroshenkoam@qameta.io>
2022-07-07 20:24:11 +0300

    Initial commit

---
+++ src/test/java/io/qameta/allure/IssuesWebTest.java
@@ -0,0 +1,47 @@
+package io.qameta.allure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
+
+/**
+ * @author eroshenkoam (Artem Eroshenko).
+ */
+@Layer("web")
+@Owner("eroshenkoam")
+@Feature("Issues")
+public class IssuesWebTest {
+
+    private static final String OWNER = "allure-framework";
+    private static final String REPO = "allure2";
+
+    private static final String ISSUE_TITLE = "Some issue title here";
+
+    @Test
+    @JiraIssue("AE-1")
+    @Story("Create new issue")
+    @Tags({@Tag("web"), @Tag("critical")})
+    @DisplayName("Creating new issue authorized user")
+    public void shouldCreateIssue() {
+        step(format("Open issues page `%s/%s`", OWNER, REPO));
+        step(format("Create issue with title `%s`", ISSUE_TITLE));
+        step(format("Should see issue with title `%s`", ISSUE_TITLE));
+    }
+
+    @Test
+    @JiraIssue("AE-1")
+    @Story("Close existing issue")
+    @Tags({@Tag("web"), @Tag("regress")})
+    @DisplayName("Closing new issue for authorized user")
+    public void shouldCloseIssue() {
+        step(format("Open issues page `%s/%s`", OWNER, REPO));
+        step(format("Create issue with title `%s`", ISSUE_TITLE));
+        step(format("Close issue with title `%s`", ISSUE_TITLE));
+        step(format("Should not see issue with title `%s`", ISSUE_TITLE));
+    }
+
+}
---
+++ src/test/java/io/qameta/allure/PullRequestsWebTest.java
@@ -0,0 +1,47 @@
+package io.qameta.allure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
+
+/**
+ * @author eroshenkoam (Artem Eroshenko).
+ */
+@Layer("web")
+@Owner("eroshenkoam")
+@Feature("Pull Requests")
+public class PullRequestsWebTest {
+
+    private static final String OWNER = "allure-framework";
+    private static final String REPO = "allure2";
+
+    private static final String BRANCH = "new-feature";
+
+    @Test
+    @JiraIssue("AE-2")
+    @Story("Create new pull request")
+    @Tags({@Tag("web"), @Tag("regress"), @Tag("smoke")})
+    @DisplayName("Creating new issue for authorized user")
+    public void shouldCreatePullRequest() {
+        step(format("Open pull requests page `%s/%s`", OWNER, REPO));
+        step(format("Create pull request with title `%s`", BRANCH));
+        step(format("Should see pull request with title `%s`", BRANCH));
+    }
+
+    @Test
+    @JiraIssue("AE-2")
+    @Story("Close existing pull request")
+    @Tags({@Tag("web"), @Tag("regress")})
+    @DisplayName("Deleting existing issue for authorized user")
+    public void shouldClosePullRequest() {
+        step(format("Open pull requests page `%s/%s`", OWNER, REPO));
+        step(format("Create pull request with title `%s`", BRANCH));
+        step(format("Close pull request with title `%s`", BRANCH));
+        step(format("Should not see pull request with title `%s`", BRANCH));
+    }
+
+}
